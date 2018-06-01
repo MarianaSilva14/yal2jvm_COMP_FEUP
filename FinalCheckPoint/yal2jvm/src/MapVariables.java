75202ea083df19c377d4d9c4d725f82298b38359
MarianaSilva14 <mariana16095@gmail.com>
2018-06-01 11:01:28 +0100

    Init Final Checkpooint

---
+++ FinalCheckPoint/yal2jvm/src/MapVariables.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+
+public class MapVariables {
+
+    public MapVariables parent;
+    public HashMap<String,Integer> variableByteCodes = new HashMap<String,Integer>();
+    public HashMap<String,String> functionReturn = new HashMap<String,String>();
+    public int counter;
+    public int loopCounter=0;
+
+    public MapVariables(MapVariables parent){
+      counter = 0;
+      this.parent = parent;
+    }
+
+    public void putOnHashMap(String variable){
+        variableByteCodes.put(variable, counter);
+        counter++;
+    }
+
+    public int returnByteCode(String variable){
+        if(variableByteCodes.containsKey(variable))
+            return variableByteCodes.get(variable);
+
+        return -1;
+    }
+
+    public void putOnHashMapFunctionReturn(String variable, String returnType){
+        functionReturn.put(variable, returnType);
+    }
+
+    public String returnFunctionType(String variable){
+        if(parent.functionReturn.containsKey(variable))
+            return parent.functionReturn.get(variable);
+
+        return "";
+    }
+
+    public int getLoopCounter(){
+        return loopCounter;
+    }
+
+}
